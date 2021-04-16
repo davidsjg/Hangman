e10246f3d8bb8c8094462ad2fcef37723dd40a5d
Joey Davidson <j.davidson18@gmail.com>
2021-04-16 11:23:39 -0600

    forEach for letters established, random word split into characters

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,12 +1,27 @@
 let timerDisp = document.querySelector(".timer")
+let startBtn = document.querySelector("#startBtn")
+let displayDiv = document.querySelector(".display")
+let wordDisp = document.querySelector(".wordDisplay")
+const log = document.getElementById('log');
+
+document.addEventListener('keypress', logKey);
+
+function logKey(e) {
+  let keyPressed = e.key
+
+}
+
+let wordArray = ['steel', 'donkey', 'buffalo', 'avalanche', 'cereal']
+
+
 
 
 setTime()   
-// setSeconds()
 
 
 let secondsLeft = 75
 let minutes = secondsLeft/60
+
 
 function setTime() {
     // Sets interval in variable
@@ -14,20 +29,53 @@ function setTime() {
       secondsLeft--;
 
       let minutes = secondsLeft/60
-      console.log(Math.floor(minutes))
 
       let modSeconds = secondsLeft % 60
-      console.log(modSeconds)
       let seconds = modSeconds * 60 
-      console.log(seconds)
       timerDisp.textContent = Math.floor(minutes) + ":" + modSeconds
 
       if(secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
         // Calls function to create and append image
-        alert("Out of Time!")
+        // alert("Out of Time!")
+        timerDisp.textContent = ""
       }
   
     }, 1000);
 }
+
+
+let wordUnder = ""
+let under = ` _ `
+
+
+startBtn.addEventListener("click", function() {
+  displayDiv.textContent = ""
+  createRandom()
+})
+
+let createRandom = function() {
+  var currWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+  var chars = currWord.split('');
+  console.log(chars)
+  console.log(currWord)
+
+  chars.forEach(function(letter) {
+    var numLetters = chars.length
+    // var newUnder = document.createElement("p")
+    // newUnder.textContent = "_"
+    // wordDisp.appendChild(newUnder)
+
+    wordUnder += under
+
+
+    wordDisp.textContent = wordUnder
+
+  })
+
+
+}
+
+
+
